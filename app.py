afd897973db92267febc6df29a2bea2ad6ab7b8f
SongBuster <songbuster@gmail.com>
2024-11-21 19:09:53 +0100

    limpiando la app

--- app.py
+++ app.py
@@ -6,59 +6,7 @@ import pandas as pd
 import time
 import os
 
-st.title("Hola, GoalBuster!")
-# st.write("FIREBASE_PRIVATE_KEY:", st.secrets["FIREBASE_PRIVATE_KEY"])
-# st.write("FIREBASE_PROJECT_ID:", os.environ.get("FIREBASE_PROJECT_ID"))
-#st.write("API_KEY:", st.secrets["firebase_config"]["API_KEY"])
-#st.write("¡Tu app Streamlit está funcionando!")
-#st.json(st.secrets)
 
-firebase_config =  {
-    "apiKey": st.secrets["firebase_config"]["API_KEY"],
-    "authDomain": st.secrets["firebase_config"]["AUTH_DOMAIN"],
-    "databaseURL": st.secrets["firebase_config"]["DATABASE_URL"],
-    "storageBucket": st.secrets["firebase_config"]["STORAGE_BUCKET"],
-    "messagingSenderId": st.secrets["firebase_config"]["MESSAGING_SENDER_ID"],
-    "appId": st.secrets["firebase_config"]["APP_ID"],
-}
-
-firebase_creedentials = {
-    "type": st.secrets["firebase_credentials"]["FIREBASE_TYPE"], 
-    "project_id": st.secrets["firebase_credentials"]["FIREBASE_PROJECT_ID"] , 
-    "private_key_id": st.secrets["firebase_credentials"]["FIREBASE_PRIVATE_KEY_ID"] ,
-    "private_key": st.secrets["firebase_credentials"]["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n"),
-    "client_email": st.secrets["firebase_credentials"]["FIREBASE_CLIENT_EMAIL"],
-    "client_id": st.secrets["firebase_credentials"]["FIREBASE_CLIENT_ID"],
-    "token_uri" : st.secrets["firebase_credentials"]["TOKEN_URI"],
-    "auth_provider_x509_cert_url" : st.secrets["firebase_credentials"]["AUTH_X509_CERT"],
-    "client_x509_cert_url" : st.secrets["firebase_credentials"]["CLIENT_X509_CERT"],
-    "universe_domain" : st.secrets["firebase_credentials"]["UNIVERSE_DOMAIN"],
-}
-
-firebase = pyrebase.initialize_app(firebase_config)
-auth = firebase.auth()
-
-# Inicializa Firestore con Firebase Admin
-@st.cache_resource
-def init_firestore():
-    cred = credentials.Certificate(firebase_creedentials)  # Ruta a tu archivo JSON de credenciales
-    firebase_admin.initialize_app(cred)
-    return firestore.client()
-
-db = init_firestore()
-
-# Interfaz de Streamlit
-#st.title("Autenticación y Juegos del Usuario")
-
-# # Autenticación del usuario
-# email = st.text_input("Correo", placeholder="Introduce tu correo")
-# password = st.text_input("Contraseña", type="password", placeholder="Introduce tu contraseña")
-email = "handballlivestats@gmail.com"
-password = "d43HANn3rys"
-game_id = "DIRECTO"
-
-valores_exito = {"GOL","PARADA","FALLO RIVAL","GOL+ASISTENCIA","EXITO"}
-valores_fallo = {"FRACASO","GOL RIVAL","PARADA RIVAL","FALLO"}
 def style_rows(df):
     def highlight_row(row):
         # Cambiar el color de la fila si el atributo es 'final'
@@ -128,10 +76,52 @@ def print_actions():
         st.warning("No se encontraron acciones para este juego.")
 
 
+
+firebase_config =  {
+    "apiKey": st.secrets["firebase_config"]["API_KEY"],
+    "authDomain": st.secrets["firebase_config"]["AUTH_DOMAIN"],
+    "databaseURL": st.secrets["firebase_config"]["DATABASE_URL"],
+    "storageBucket": st.secrets["firebase_config"]["STORAGE_BUCKET"],
+    "messagingSenderId": st.secrets["firebase_config"]["MESSAGING_SENDER_ID"],
+    "appId": st.secrets["firebase_config"]["APP_ID"],
+}
+
+firebase_creedentials = {
+    "type": st.secrets["firebase_credentials"]["FIREBASE_TYPE"], 
+    "project_id": st.secrets["firebase_credentials"]["FIREBASE_PROJECT_ID"] , 
+    "private_key_id": st.secrets["firebase_credentials"]["FIREBASE_PRIVATE_KEY_ID"] ,
+    "private_key": st.secrets["firebase_credentials"]["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n"),
+    "client_email": st.secrets["firebase_credentials"]["FIREBASE_CLIENT_EMAIL"],
+    "client_id": st.secrets["firebase_credentials"]["FIREBASE_CLIENT_ID"],
+    "token_uri" : st.secrets["firebase_credentials"]["TOKEN_URI"],
+    "auth_provider_x509_cert_url" : st.secrets["firebase_credentials"]["AUTH_X509_CERT"],
+    "client_x509_cert_url" : st.secrets["firebase_credentials"]["CLIENT_X509_CERT"],
+    "universe_domain" : st.secrets["firebase_credentials"]["UNIVERSE_DOMAIN"],
+}
+
+firebase = pyrebase.initialize_app(firebase_config)
+auth = firebase.auth()
+
+# Inicializa Firestore con Firebase Admin
+@st.cache_resource
+def init_firestore():
+    cred = credentials.Certificate(firebase_creedentials)  # Ruta a tu archivo JSON de credenciales
+    firebase_admin.initialize_app(cred)
+    return firestore.client()
+
+db = init_firestore()
+
+email = "handballlivestats@gmail.com"
+password = "d43HANn3rys"
+game_id = "DIRECTO"
+
+valores_exito = {"GOL","PARADA","FALLO RIVAL","GOL+ASISTENCIA","EXITO"}
+valores_fallo = {"FRACASO","GOL RIVAL","PARADA RIVAL","FALLO"}
+
 try:
     # Autenticar usuario
     user = auth.sign_in_with_email_and_password(email, password)
-    st.success(f"Autenticado como: {user['email']}")
+    #st.success(f"Autenticado como: {user['email']}")
 
     # Obtener el UID del usuario autenticado
     user_id = user['localId']  # Este es el UID único del usuario en Firebase Authentication
